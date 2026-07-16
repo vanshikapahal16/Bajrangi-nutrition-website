87ae4e5be3df087dfb803b53bea208a44c4a3e8f
akashyadav1284 <akashyadav9992462520@gmail.com>
2026-07-16 18:04:03 +0530

    style: shift 3D canister upward to avoid overflowing bottom border frame

--- src/components/Scene3D.tsx
+++ src/components/Scene3D.tsx
@@ -65,7 +65,7 @@ export default function Scene3D({ activeCategory = "all" }: Scene3DProps) {
         <SupplementJar3D
           category="Protein"
           image="/assets/whey_isolate.png"
-          position={[0, -0.7, 0]}
+          position={[0, -0.15, 0]}
           rotation={[0.05, -Math.PI / 12, 0]}
           scale={1.15}
           hovered={hovered}
